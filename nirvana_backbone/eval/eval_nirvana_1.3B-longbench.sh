fff2c99bbbfa075bd9dda3de79b31d06ae2e2d1e
Yuhua Jiang <jiangyh221600@gmail.com>
2025-09-24 13:18:03 +0800

    Update eval_nirvana_1.3B-longbench.sh

--- nirvana_backbone/eval/eval_nirvana_1.3B-longbench.sh
+++ nirvana_backbone/eval/eval_nirvana_1.3B-longbench.sh
@@ -7,7 +7,7 @@ export HF_ENDPOINT="https://hf-mirror.com"
 
 suffix=29
 model_path="./nirvana-1_3B-${suffix}/hf-95500"
-tokenizer_path="/cpfs02/shared/llmit6/liudawei/models/Llama-2-7b-chat-hf"
+tokenizer_path="<YOUR_PATH>"
 cp ${tokenizer_path}/tokenizer_config.json "$model_path"
 cp ${tokenizer_path}/tokenizer.json "$model_path"
 cp ${tokenizer_path}/tokenizer.model "$model_path"
